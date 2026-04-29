ac3b1991394e66e1c285c30878a14056a23b5c3b
alfa199845-crypto <alfa199845@gmail.com>
2026-04-29 08:58:58 -0500

    Create taylor.h

---
+++ taylor.h
@@ -0,0 +1,11 @@
+#ifndef TAYLOR_H
+#define TAYLOR_H
+
+// Prototipos de las funciones
+double taylor_exponencial(double x, int n);
+double taylor_seno(double x, int n);
+double taylor_coseno(double x, int n);
+double taylor_tangente(double x, int n);
+double taylor_raiz(double x, int n);
+
+#endif
